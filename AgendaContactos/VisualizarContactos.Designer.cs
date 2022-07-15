5ae065bbf8eae78ba11283b3b109e65319f4d754
PAZZIS ALEJANDRO PAULINO BATISTA <1103790@est.intec.edu.do>
2022-07-14 22:08:46 -0400

    Nombre de formulario cambiado a VisualizarContactos

--- AgendaContactos/VisualizarContactos.Designer.cs
+++ AgendaContactos/VisualizarContactos.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AgendaContactos
 {
-    partial class Form1
+    partial class VisualizarContactos
     {
         /// <summary>
         /// Required designer variable.
--- AgendaContactos/VisualizarContactos.cs
+++ AgendaContactos/VisualizarContactos.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace AgendaContactos
 {
-    public partial class Form1 : Form
+    public partial class VisualizarContactos : Form
     {
-        public Form1()
+        public VisualizarContactos()
         {
             InitializeComponent();
         }
