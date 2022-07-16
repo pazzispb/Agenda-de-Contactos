68ee285b9dc1f9f9b4fc5fe8b5a4f3eda6507e3f
PAZZIS ALEJANDRO PAULINO BATISTA <1103790@est.intec.edu.do>
2022-07-15 20:48:24 -0400

    Menu principal implementado

---
+++ AgendaContactos/MenuPrincipal.designer.cs
@@ -0,0 +1,195 @@
+﻿namespace AgendaContactos
+{
+    partial class MenuPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelSideMenu = new System.Windows.Forms.Panel();
+            this.btnCategorias = new FontAwesome.Sharp.IconButton();
+            this.btnContactos = new FontAwesome.Sharp.IconButton();
+            this.btnPaginaPrincipal = new FontAwesome.Sharp.IconButton();
+            this.panePhoto = new System.Windows.Forms.Panel();
+            this.btnMenu = new FontAwesome.Sharp.IconButton();
+            this.panelFormHijo = new System.Windows.Forms.Panel();
+            this.panelSideMenu.SuspendLayout();
+            this.panePhoto.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelSideMenu
+            // 
+            this.panelSideMenu.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panelSideMenu.Controls.Add(this.btnCategorias);
+            this.panelSideMenu.Controls.Add(this.btnContactos);
+            this.panelSideMenu.Controls.Add(this.btnPaginaPrincipal);
+            this.panelSideMenu.Controls.Add(this.panePhoto);
+            this.panelSideMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelSideMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelSideMenu.Margin = new System.Windows.Forms.Padding(4);
+            this.panelSideMenu.Name = "panelSideMenu";
+            this.panelSideMenu.Size = new System.Drawing.Size(307, 598);
+            this.panelSideMenu.TabIndex = 0;
+            // 
+            // btnCategorias
+            // 
+            this.btnCategorias.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCategorias.FlatAppearance.BorderSize = 0;
+            this.btnCategorias.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCategorias.Font = new System.Drawing.Font("Montserrat Medium", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCategorias.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnCategorias.IconChar = FontAwesome.Sharp.IconChar.Heart;
+            this.btnCategorias.IconColor = System.Drawing.Color.White;
+            this.btnCategorias.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnCategorias.IconSize = 30;
+            this.btnCategorias.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCategorias.Location = new System.Drawing.Point(0, 295);
+            this.btnCategorias.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCategorias.Name = "btnCategorias";
+            this.btnCategorias.Padding = new System.Windows.Forms.Padding(13, 0, 0, 0);
+            this.btnCategorias.Size = new System.Drawing.Size(307, 65);
+            this.btnCategorias.TabIndex = 7;
+            this.btnCategorias.Tag = "Categorias";
+            this.btnCategorias.Text = "Categorias";
+            this.btnCategorias.UseVisualStyleBackColor = true;
+            // 
+            // btnContactos
+            // 
+            this.btnContactos.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnContactos.FlatAppearance.BorderSize = 0;
+            this.btnContactos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnContactos.Font = new System.Drawing.Font("Montserrat Medium", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnContactos.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnContactos.IconChar = FontAwesome.Sharp.IconChar.PeopleGroup;
+            this.btnContactos.IconColor = System.Drawing.Color.White;
+            this.btnContactos.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnContactos.IconSize = 30;
+            this.btnContactos.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnContactos.Location = new System.Drawing.Point(0, 230);
+            this.btnContactos.Margin = new System.Windows.Forms.Padding(4);
+            this.btnContactos.Name = "btnContactos";
+            this.btnContactos.Padding = new System.Windows.Forms.Padding(13, 0, 0, 0);
+            this.btnContactos.Size = new System.Drawing.Size(307, 65);
+            this.btnContactos.TabIndex = 6;
+            this.btnContactos.Tag = "Contactos";
+            this.btnContactos.Text = "Contactos";
+            this.btnContactos.UseVisualStyleBackColor = true;
+            // 
+            // btnPaginaPrincipal
+            // 
+            this.btnPaginaPrincipal.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPaginaPrincipal.FlatAppearance.BorderSize = 0;
+            this.btnPaginaPrincipal.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnPaginaPrincipal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPaginaPrincipal.Font = new System.Drawing.Font("Montserrat Medium", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPaginaPrincipal.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnPaginaPrincipal.IconChar = FontAwesome.Sharp.IconChar.Home;
+            this.btnPaginaPrincipal.IconColor = System.Drawing.Color.White;
+            this.btnPaginaPrincipal.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnPaginaPrincipal.IconSize = 30;
+            this.btnPaginaPrincipal.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPaginaPrincipal.Location = new System.Drawing.Point(0, 165);
+            this.btnPaginaPrincipal.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPaginaPrincipal.Name = "btnPaginaPrincipal";
+            this.btnPaginaPrincipal.Padding = new System.Windows.Forms.Padding(13, 0, 0, 0);
+            this.btnPaginaPrincipal.Size = new System.Drawing.Size(307, 65);
+            this.btnPaginaPrincipal.TabIndex = 5;
+            this.btnPaginaPrincipal.Tag = "Pagina Principal";
+            this.btnPaginaPrincipal.Text = "Pagina Principal";
+            this.btnPaginaPrincipal.UseVisualStyleBackColor = true;
+            this.btnPaginaPrincipal.Click += new System.EventHandler(this.btnPaginaPrincipal_Click);
+            // 
+            // panePhoto
+            // 
+            this.panePhoto.Controls.Add(this.btnMenu);
+            this.panePhoto.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panePhoto.Location = new System.Drawing.Point(0, 0);
+            this.panePhoto.Margin = new System.Windows.Forms.Padding(4);
+            this.panePhoto.Name = "panePhoto";
+            this.panePhoto.Size = new System.Drawing.Size(307, 165);
+            this.panePhoto.TabIndex = 0;
+            // 
+            // btnMenu
+            // 
+            this.btnMenu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnMenu.FlatAppearance.BorderSize = 0;
+            this.btnMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMenu.Font = new System.Drawing.Font("Montserrat Medium", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMenu.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnMenu.IconChar = FontAwesome.Sharp.IconChar.BarChart;
+            this.btnMenu.IconColor = System.Drawing.Color.White;
+            this.btnMenu.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnMenu.IconSize = 30;
+            this.btnMenu.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMenu.Location = new System.Drawing.Point(0, 0);
+            this.btnMenu.Margin = new System.Windows.Forms.Padding(4);
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Padding = new System.Windows.Forms.Padding(13, 0, 0, 0);
+            this.btnMenu.Size = new System.Drawing.Size(307, 165);
+            this.btnMenu.TabIndex = 2;
+            this.btnMenu.Tag = "Menu";
+            this.btnMenu.Text = "Menu";
+            this.btnMenu.UseVisualStyleBackColor = true;
+            this.btnMenu.Click += new System.EventHandler(this.btnMenu_Click);
+            // 
+            // panelFormHijo
+            // 
+            this.panelFormHijo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelFormHijo.Location = new System.Drawing.Point(307, 0);
+            this.panelFormHijo.Margin = new System.Windows.Forms.Padding(4);
+            this.panelFormHijo.Name = "panelFormHijo";
+            this.panelFormHijo.Size = new System.Drawing.Size(820, 598);
+            this.panelFormHijo.TabIndex = 1;
+            // 
+            // MenuPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1127, 598);
+            this.Controls.Add(this.panelFormHijo);
+            this.Controls.Add(this.panelSideMenu);
+            this.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MenuPrincipal";
+            this.Text = "Form1";
+            this.panelSideMenu.ResumeLayout(false);
+            this.panePhoto.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelSideMenu;
+        private System.Windows.Forms.Panel panePhoto;
+        private FontAwesome.Sharp.IconButton btnMenu;
+        private FontAwesome.Sharp.IconButton btnCategorias;
+        private FontAwesome.Sharp.IconButton btnContactos;
+        private FontAwesome.Sharp.IconButton btnPaginaPrincipal;
+        private System.Windows.Forms.Panel panelFormHijo;
+    }
+}
+
