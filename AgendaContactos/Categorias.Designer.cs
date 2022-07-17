ffc1f7c818eae8564645ce97e56595ff6ca6f9ca
PAZZIS ALEJANDRO PAULINO BATISTA <1103790@est.intec.edu.do>
2022-07-17 11:42:35 -0400

    Diseño arreglado en categorias

--- AgendaContactos/Categorias.Designer.cs
+++ AgendaContactos/Categorias.Designer.cs
@@ -58,7 +58,7 @@
             this.gbDatos.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.gbDatos.Location = new System.Drawing.Point(28, 67);
             this.gbDatos.Name = "gbDatos";
-            this.gbDatos.Size = new System.Drawing.Size(544, 146);
+            this.gbDatos.Size = new System.Drawing.Size(567, 148);
             this.gbDatos.TabIndex = 0;
             this.gbDatos.TabStop = false;
             this.gbDatos.Text = "Datos de la categoria";
@@ -71,10 +71,10 @@
             this.txtDescripcion.BackColor = System.Drawing.SystemColors.Menu;
             this.txtDescripcion.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtDescripcion.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtDescripcion.Location = new System.Drawing.Point(105, 77);
+            this.txtDescripcion.Location = new System.Drawing.Point(124, 77);
             this.txtDescripcion.Multiline = true;
             this.txtDescripcion.Name = "txtDescripcion";
-            this.txtDescripcion.Size = new System.Drawing.Size(406, 49);
+            this.txtDescripcion.Size = new System.Drawing.Size(410, 51);
             this.txtDescripcion.TabIndex = 4;
             // 
             // label2
@@ -86,7 +86,7 @@
             this.label2.Font = new System.Drawing.Font("Montserrat SemiBold", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(17, 77);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(79, 16);
+            this.label2.Size = new System.Drawing.Size(101, 21);
             this.label2.TabIndex = 3;
             this.label2.Text = "Descripción";
             // 
@@ -99,7 +99,7 @@
             this.chkIsVisible.Font = new System.Drawing.Font("Montserrat SemiBold", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.chkIsVisible.Location = new System.Drawing.Point(418, 38);
             this.chkIsVisible.Name = "chkIsVisible";
-            this.chkIsVisible.Size = new System.Drawing.Size(93, 20);
+            this.chkIsVisible.Size = new System.Drawing.Size(119, 25);
             this.chkIsVisible.TabIndex = 2;
             this.chkIsVisible.Text = "¿Es visible?";
             this.chkIsVisible.UseVisualStyleBackColor = true;
@@ -113,7 +113,7 @@
             this.txtNombre.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtNombre.Location = new System.Drawing.Point(105, 36);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(280, 22);
+            this.txtNombre.Size = new System.Drawing.Size(280, 19);
             this.txtNombre.TabIndex = 1;
             // 
             // label1
@@ -125,7 +125,7 @@
             this.label1.Font = new System.Drawing.Font("Montserrat SemiBold", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(17, 37);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 16);
+            this.label1.Size = new System.Drawing.Size(73, 21);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nombre";
             // 
@@ -223,7 +223,7 @@
             // 
             // Categorias
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(615, 486);
