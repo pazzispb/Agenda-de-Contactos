34834e73440d7022ea82bf38c2a6eff765ccef49
Johan Contreras <Cepedaabiel@gmail.com>
2022-07-18 01:08:44 -0400

    Comentarios

--- AgendaContactos/InformacionContacto.cs
+++ AgendaContactos/InformacionContacto.cs
@@ -18,19 +18,19 @@ namespace AgendaContactos
         {
             InitializeComponent();
             this.id = id;
-            EstadoInicial();
+            EstadoInicial(); 
         }
 
-        void EstadoInicial()
+        void EstadoInicial() // Este es el estado en el que se presentara el form
         {
             var json = new Json();
-            Contacto contacto = json.ObtenerContactos().FirstOrDefault(x => x.Id == id);
+            Contacto contacto = json.ObtenerContactos().FirstOrDefault(x => x.Id == id); // Filtramos el contacto por el Id para tener sus datos y presentarlos por pantalla
             txtBoxNombre.Text = contacto.Nombres;
             txtBoxApellido.Text = contacto.Apellidos;
             maskedTxtBoxTelefonoPersonal.Text = contacto.TelefonoPersonal;
             maskedTxtBoxTelefonoResidencial.Text = contacto.TelefonoResidencial;
             maskedTxtBoxTelefonoTrabajo.Text = contacto.TelefonoTrabajo;
-            cbCategoria.Text = contacto.Categoria;
+            cbCategoria.Text = contacto.Categoria; // Cada dato se va a mostrar con el equivalente al dato ya registrado del contacto
             dtpNacimiento.Value = contacto.FechaNacimiento;
             txtBoxCorreoElectronico.Text = contacto.CorreoElectronico;
             txtBoxDescripcion.Text = contacto.Descripcion;
@@ -40,12 +40,12 @@ namespace AgendaContactos
         }
 
 
-        bool ValidarCamposObligatorios()
+        bool ValidarCamposObligatorios() //confirmara los campos vacios 
         {
             return (String.IsNullOrWhiteSpace(txtBoxNombre.Text));
         }
 
-        bool ValidarNombreUnico(string nombre)
+        bool ValidarNombreUnico(string nombre) //confirmara que no exista otra persona igual en la lista de contactos
         {
             var json = new Json();
             if (json.ObtenerContactos() == null) return true;
@@ -57,12 +57,12 @@ namespace AgendaContactos
         {
 
 
-            if (ValidarCamposObligatorios()) 
+            if (ValidarCamposObligatorios()) // En caso de campo vacio, mostrara mensaje por pantalla un mensaje de alerta
             {
                 MessageBox.Show("Rellene los campos vacios", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
             }
-            if (!ValidarNombreUnico(txtBoxNombre.Text)) 
+            if (!ValidarNombreUnico(txtBoxNombre.Text)) // En caso de que se escriba una persona ya colocada anteriormente, mandara mensaje de alerta
             {
                 MessageBox.Show("El nombre que introdujo ya existe", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
@@ -71,16 +71,16 @@ namespace AgendaContactos
             var json = new Json();
             var listadoContactos = json.ObtenerContactos();
             Contacto contacto = listadoContactos.FirstOrDefault(x => x.Id == id);
-            var index = listadoContactos.IndexOf(contacto);
+            var index = listadoContactos.IndexOf(contacto); // Tomara la lista de contactos junto con el contacto ubicado por el id, y obtendremos el index
             listadoContactos[index].Nombres = txtBoxNombre.Text;
             listadoContactos[index].Apellidos = txtBoxApellido.Text;
             listadoContactos[index].TelefonoPersonal = maskedTxtBoxTelefonoPersonal.Text;
-            listadoContactos[index].TelefonoResidencial = maskedTxtBoxTelefonoResidencial.Text;
+            listadoContactos[index].TelefonoResidencial = maskedTxtBoxTelefonoResidencial.Text; //en cada uno de estos, guardamos lo que sea que se actualice 
             listadoContactos[index].TelefonoTrabajo = maskedTxtBoxTelefonoTrabajo.Text;
             listadoContactos[index].Categoria = cbCategoria.Text;
             listadoContactos[index].FechaNacimiento = dtpNacimiento.Value;
             listadoContactos[index].CorreoElectronico = txtBoxCorreoElectronico.Text;
-            listadoContactos[index].Descripcion = txtBoxDescripcion.Text;
+            listadoContactos[index].Descripcion = txtBoxDescripcion.Text; // En cada uno de estos, guardamos lo que sea que se actualice
             listadoContactos[index].Apodo = txtBoxApodo.Text;
             if (checkBoxIsFavorito.Checked)
             {
@@ -91,9 +91,9 @@ namespace AgendaContactos
                 listadoContactos[index].isEmergencia = true;
             }
 
-            json.GuardarContactos(listadoContactos);
+            json.GuardarContactos(listadoContactos); // Se guardaran los cambios al Json
             MessageBox.Show("Cambios guardados con exito", "EXITO", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            Program.aplicacion.abrirSubFormulario(new VisualizarContactos());
+            Program.aplicacion.abrirSubFormulario(new VisualizarContactos()); // Regresara a la pantalla de inicio
         }
         private void bttnEliminar_Click(object sender, EventArgs e)
         {
@@ -101,9 +101,9 @@ namespace AgendaContactos
             var listadoContactos = json.ObtenerContactos();
             Contacto contacto = listadoContactos.FirstOrDefault(x => x.Id == id);
             var index = listadoContactos.IndexOf(contacto);
-            listadoContactos.Remove(contacto);
-            json.GuardarContactos(listadoContactos);
-            Program.aplicacion.abrirSubFormulario(new VisualizarContactos());
+            listadoContactos.Remove(contacto); // Tomara el contacto y lo eliminara de la lista
+            json.GuardarContactos(listadoContactos); // Se guardaran los cambios en el Json
+            Program.aplicacion.abrirSubFormulario(new VisualizarContactos()); // Regresara a la pantalla de inicio
 
         }
     }
